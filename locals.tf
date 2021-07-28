1b2f8e92ab508cdb0973c710200649994eddcd71
amit tsarfaty <amit.tsarfaty@imperva.com>
2021-07-28 15:42:33 +0300

    added volume module

--- locals.tf
+++ locals.tf
@@ -1,21 +1,25 @@
 
 locals {
   deployments = {
-    # nodered = {
-    #   container_count = length(var.external_port["nodered"][terraform.workspace])
-    #   image           = var.image["nodered"][terraform.workspace]
-    #   in_port         = var.internal_port["nodered"].value
-    #   ext_port        = var.external_port["nodered"][terraform.workspace]
-    #   container_path  = "/data"
-    # }
-    # influxdb = {
-    #   image           = var.image["influxdb"][terraform.workspace]
-    #   container_count = length(var.external_port["influxdb"][terraform.workspace])
-    #   image           = var.image["influxdb"][terraform.workspace]
-    #   in_port         = var.internal_port["influxdb"].value
-    #   ext_port        = var.external_port["influxdb"][terraform.workspace]
-    #   container_path  = "/var/lib/influxdb"
-    # }
+    nodered = {
+      container_count = length(var.external_port["nodered"][terraform.workspace])
+      image           = var.image["nodered"][terraform.workspace]
+      in_port         = var.internal_port["nodered"].value
+      ext_port        = var.external_port["nodered"][terraform.workspace]
+      volumes = [
+        {container_path_each  = "/data"}
+      ]
+    }
+    influxdb = {
+      image           = var.image["influxdb"][terraform.workspace]
+      container_count = length(var.external_port["influxdb"][terraform.workspace])
+      image           = var.image["influxdb"][terraform.workspace]
+      in_port         = var.internal_port["influxdb"].value
+      ext_port        = var.external_port["influxdb"][terraform.workspace]      
+      volumes = [
+        {container_path_each  = "/var/lib/influxdb"}
+      ]
+    }
     grafana = {
       image           = var.image["grafana"][terraform.workspace]
       container_count = length(var.external_port["grafana"][terraform.workspace])
